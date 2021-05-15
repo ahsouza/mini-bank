9a554bb2327360e8d503b851d4e38a450213eb4a
ahsouza <annibalhsouza@gmail.com>
2021-05-15 19:28:52 -0300

    feat: acrescentar novas configurações de testes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  roots: ['<rootDir>/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/./**/*.ts',
+    '!<rootDir>/./main/**'
+  ]
 }

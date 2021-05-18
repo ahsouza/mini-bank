aef17f1251133bf0bebce46a1ac01c739b5adc3e
ahsouza <annibalhsouza@gmail.com>
2021-05-18 00:00:22 -0300

    refactor: removing unnecessary file and mass structure for clean architecture

--- domain/domain/usecases/client/add-client.ts
+++
@@ -1,9 +0,0 @@
-import { ClientModel } from '@/domain/models'
-
-export interface AddClient {
-  add: (data: AddClient.Params) => Promise<void>
-}
-
-export namespace AddClient {
-  export type Params = Omit<ClientModel, 'id'>
-}
--- domain/domain/usecases/extract/add-extract.ts
+++
@@ -1,9 +0,0 @@
-import { ExtractModel } from '@/domain/models'
-
-export interface AddExtract {
-  add: (data: AddExtract.Params) => Promise<void>
-}
-
-export namespace AddExtract {
-  export type Params = Omit<ExtractModel, 'id'>
-}

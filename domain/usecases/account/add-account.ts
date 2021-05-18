9ecabac60eea06ea02b3bbfa64318be513d18d9e
ahsouza <annibalhsouza@gmail.com>
2021-05-18 16:13:06 -0300

    refactor: erasing unnecessary structure

--- domain/usecases/account/add-account.ts
+++
@@ -1,9 +0,0 @@
-import { AccountModel } from '@/domain/models'
-
-export interface AddAccount {
-  add: (data: AddAccount.Params) => Promise<void>
-}
-
-export namespace AddAccount {
-  export type Params = Omit<AccountModel, 'id'>
-}
--- domain/usecases/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './client/add-client'
-export * from './institution/add-institution'
-export * from './extract/add-extract'
-export * from './account/add-account'

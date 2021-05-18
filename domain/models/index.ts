e7707e5aaa0d69531f79dcc6027a372a5a0b9d51
ahsouza <annibalhsouza@gmail.com>
2021-05-17 23:24:53 -0300

    feat: exporting models through the main file index.ts

---
+++ domain/models/index.ts
@@ -0,0 +1,8 @@
+  
+export * from './extract'
+export * from './client'
+export * from './institution'
+export * from './transaction-type'
+export * from './account'
+export * from './account-status'
+export * from './account-type'

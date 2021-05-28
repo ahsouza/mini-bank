1fb92070046e94ab4aebd5593504f931b1bff968
ahsouza <annibalhsouza@gmail.com>
2021-05-28 19:08:16 -0300

    refactor: refactor FileUploader

--- api/presentation/protocols/file-uploader.ts
+++ api/presentation/protocols/file-uploader.ts
@@ -1,7 +1,5 @@
 import { File, UploadedFile } from '@/domain/models'
 
 export interface FileUploader {
-  upload: (
-    files: File | File[]
-  ) => Promise<UploadedFile | UploadedFile[] | undefined>
+  upload: (files: File | File[]) => Promise<UploadedFile | UploadedFile[] | undefined>
 }

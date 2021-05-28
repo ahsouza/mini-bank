2a8f9629767f0bb9024e48f0b68ff21c2aede8cb
ahsouza <annibalhsouza@gmail.com>
2021-05-28 19:20:47 -0300

    refactor: refactoring of in the remote file for uploading files

--- api/data/usecases/file/remote-file-upload.ts
+++ api/data/usecases/file/remote-file-upload.ts
@@ -1,25 +1,22 @@
-import { inject, injectable } from "tsyringe"
-import { File, UploadedFile } from "@/domain/models"
-import { FileUpload } from "@/domain/usecases"
-import { FileUploader, DirectPublisher } from "@/presentation/protocols"
-import { FileUploadError } from "@/domain/errors"
+import { inject, injectable } from 'tsyringe'
+import { File, UploadedFile } from '@/domain/models'
+import { FileUpload } from '@/domain/usecases'
+import { FileUploader } from '@/presentation/protocols'
+import { FileUploadInvalid } from '@/presentation/errors'
 
 @injectable()
 export class RemoteFileUpload implements FileUpload {
-  constructor(
-    @inject("FileUploader")
-    private readonly fileUploader: FileUploader,
-    @inject("DirectPublisher")
-    private readonly directPublisher: DirectPublisher
+  constructor (
+    @inject('FileUploader')
+    private readonly fileUploader: FileUploader
   ) {}
 
-  async upload(files: File[]): Promise<UploadedFile[]> {
+  async upload (files: File[]): Promise<UploadedFile[]> {
     const uploadedFiles = await this.fileUploader.upload(files)
 
     if (!uploadedFiles) {
-      throw new FileUploadError()
+      throw new FileUploadInvalid()
     }
-    await this.directPublisher.publishDirect("files.uploaded", uploadedFiles)
 
     return uploadedFiles as UploadedFile[]
   }

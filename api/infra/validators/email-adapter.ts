46e281c003d13dd906f3e26e21f77b55b7212750
ahsouza <annibalhsouza@gmail.com>
2021-05-29 07:49:52 -0300

    refactor: change module path

--- api/infra/validators/email-adapter.ts
+++ api/infra/validators/email-adapter.ts
@@ -1,4 +1,4 @@
-import { EmailValidator } from '@/validation/protocols'
+import { EmailValidator } from '@/utils/protocols'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {

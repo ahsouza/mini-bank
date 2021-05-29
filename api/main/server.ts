6cd91c12813bc5ac6fa76b840c9c3fbbde711e35
ahsouza <annibalhsouza@gmail.com>
2021-05-29 08:25:34 -0300

    feat: connect and integrate mongodb to the express server

--- api/main/server.ts
+++ api/main/server.ts
@@ -1,5 +1,10 @@
 import env from '@/main/config/env'
 import 'module-alias/register'
+import { MongoHelper } from '@/infra/database'
 
-const app = (await import('./config/app')).default
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
